e59806d73c029b2a771d0a7dde12e1a52812c624
jerlibgnzlz <jerlibgnzlz@gmail.com>
2024-05-02 20:33:22 -0300

    first commit

---
+++ components/Header.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+
+
+
+const Header = () => (
+    <Text style={styles.encabezado}>
+        Criptomonedas
+    </Text>
+)
+
+
+
+const styles = StyleSheet.create({
+    encabezado: {
+        paddingBottom: 30,
+        padding: 50,
+        textTransform: "uppercase",
+        backgroundColor: 'purple',
+        textAlign: 'center',
+        color: "white",
+        fontSize: 30
+
+    },
+});
+
+
+export default Header
