e6f146aedb81cab8b1bde1be28ea7e301cc7be5d
jerlibgnzlz <jerlibgnzlz@gmail.com>
2024-05-06 20:25:17 -0300

    feature imagen

--- App.js
+++ App.js
@@ -1,15 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+import { Image, StyleSheet, Text, View } from 'react-native';
 import Header from './components/Header';
 
 export default function App () {
   return (
     <>
       <Header />
+
+      <Image
+        style={styles.imagen}
+        source={require("./assets/img/cryptomonedas.png")}
+      />
     </>
   );
 }
 
 const styles = StyleSheet.create({
-
+  imagen: {
+    width: "100%",
+    height: 150,
+    marginHorizontal: 5,
+    marginVertical: 10
+  }
 });
