05b97b6ffd27f7a0f0df8286443b13f5992bd6ef
jerlibgnzlz <jerlibgnzlz@gmail.com>
2024-05-07 00:25:01 -0300

    feature formulario

--- components/Formulario.jsx
+++ components/Formulario.jsx
@@ -1,14 +1,39 @@
 import { Text, StyleSheet, View } from 'react-native'
 import { Picker } from '@react-native-picker/picker';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 export const Formulario = () => {
+
+    const [moneda, setMoneda] = useState("")
+    const [criptomoneda, setCriptomoneda] = useState("")
+    const [criptomonedas, setCriptomonedas] = useState("")
+
+    useEffect(() => {
+        const consultarAPI = async () => {
+            const URL = `https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD`
+            const resultado = await axios.get(URL)
+            setCriptomonedas(resultado.data.Data)
+        }
+
+        consultarAPI()
+    }, [])
+
+
+    const obtenerMoneda = () => {
+        setMoneda(moneda)
+    }
+
     return (
         <View>
             <Text style={styles.label}>
                 Monedas
             </Text>
 
-            <Picker>
+            <Picker
+                onValueChange={(moneda => obtenerMoneda(moneda))}
+                selectedValue={moneda}
+            >
                 <Picker.Item label="--Seleccione--" value="" />
                 <Picker.Item label="Dolar de Estados Unidos" value="USD" />
                 <Picker.Item label="Peso mexicano" value="MXN" />
@@ -19,7 +44,7 @@ export const Formulario = () => {
             <Text style={styles.label}>
                 Criptomonedas
             </Text>
-        </View>
+        </View >
     )
 }
 
